14a61f5f6f2314aba40908b8ce896a2a2486ed17
Parth Chopra <parthchopra28@gmail.com>
2014-08-20 03:12:38 -0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Checking the messages on GitHub");
+		System.out.println("Can I go in reverse?");
 	}
 }
